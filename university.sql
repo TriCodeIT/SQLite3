122f1d512df80166859db048c5df4725e0c27ce1
Tri <bayu165kusuma@gmail.com>
2020-07-02 22:16:42 +0700

    Final Challenge 14 & 15 Data Base

--- university.sql
+++ university.sql
@@ -8,6 +8,9 @@ INSERT INTO Matakuliah(id_mk,nama_matakuliah,jumlah_sks) VALUES ('MK001','Fisika
 INSERT INTO Matakuliah(id_mk,nama_matakuliah,jumlah_sks) VALUES ('MK002','Fisika Komputasi', 3);
 INSERT INTO Matakuliah(id_mk,nama_matakuliah,jumlah_sks) VALUES ('MK003','Instrumentasi Cerdas', 4);
 
+INSERT INTO Matakuliah(id_mk,nama_matakuliah,jumlah_sks) VALUES ('MK004','Data Mining', 4);
+INSERT INTO Matakuliah(id_mk,nama_matakuliah,jumlah_sks) VALUES ('MK005','Manajemen Data', 3);
+
 CREATE TABLE Jurusan
 (
     id_jr CHAR (5) PRIMARY KEY NOT NULL,
@@ -34,25 +37,56 @@ CREATE TABLE Mahasiswa
     jenis_kelamin VARCHAR (10) NOT NULL,
     umur INTEGER NOT NULL,
     id_jr CHAR (5) NOT NULL,
-    nama_jurusan VARCHAR (20) NOT NULL,
-    FOREIGN KEY (id_jr) REFERENCES Jurusan (id_jr),
-    FOREIGN KEY (nama_jurusan) REFERENCES Jurusan (nama_jurusan)
+    FOREIGN KEY (id_jr) REFERENCES Jurusan (id_jr)
 );
-INSERT INTO Mahasiswa(nim,nama_mahasiswa,alamat,jenis_kelamin,umur,id_jr,nama_jurusan) VALUES ('2018210020','TRI SUTRISNA BHAYUKUSUMA','Jalan Depok VI Antapani Kota Bandung','Laki-Laki','29','JR001','FISIKA');
+INSERT INTO Mahasiswa(nim,nama_mahasiswa,alamat,jenis_kelamin,umur,id_jr) VALUES ('2018210020','TRI SUTRISNA BHAYUKUSUMA','Jalan Depok VI Antapani Kota Bandung','Laki-Laki','22','JR001');
+INSERT INTO Mahasiswa(nim,nama_mahasiswa,alamat,jenis_kelamin,umur,id_jr) VALUES ('2018210021','JONATHAN','Jalan Cicendo II Kota Bandung','Laki-Laki','18','JR001');
+INSERT INTO Mahasiswa(nim,nama_mahasiswa,alamat,jenis_kelamin,umur,id_jr) VALUES ('2018210022','NIRMALA','Jalan Sangkuriang III Kota Bandung','Perempuan','19','JR002');
 
-CREATE TABLE Semester 
+CREATE TABLE KontrakKuliah
 (
-    smt CHAR (2) NOT NULL,
+    id_kontrak CHAR (5) PRIMARY KEY NOT NULL,
     nim CHAR (10) NOT NULL,
     id_mk CHAR (5) NOT NULL,
     id_dos CHAR (6) NOT NULL,
     nilai TEXT NOT NULL,
-    PRIMARY KEY (nim,id_mk,id_dos),
     FOREIGN KEY (nim) REFERENCES Mahasiswa (nim),
     FOREIGN KEY (id_mk) REFERENCES Matakuliah (id_mk),
     FOREIGN KEY (id_dos) REFERENCES Dosen (id_dos)
 );
-INSERT INTO Semester(smt,nim,id_mk,id_dos,nilai) VALUES ('01','2018210020','MK003','Ahmad Aminudin','A');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00001','2018210020','MK003','DOS001','A');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00002','2018210021','MK004','DOS003','B');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00003','2018210022','MK005','DOS002','A');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00004','2018210020','MK002','DOS001','C');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00005','2018210022','MK001','DOS003','A');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00006','2018210020','MK005','DOS002','A');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00007','2018210020','MK004','DOS003','B');
+
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00008','2018210021','MK003','DOS001','D');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00009','2018210020','MK005','DOS002','E');
+INSERT INTO KontrakKuliah(id_kontrak,nim,id_mk,id_dos,nilai) VALUES ('00010','2018210022','MK004','DOS003','E');
 
 
+--NO.1 Tampilkan Seluruh Data Mahasiswa beserta Nama Jurusannya
+SELECT * FROM Mahasiswa NATURAL JOIN Jurusan WHERE Jurusan.id_jr = Mahasiswa.id_jr;
 
+--NO.2 Tampilkan Mahasiswa yang memiliki umur dibawah 20 tahun
+SELECT nama_mahasiswa,umur FROM Mahasiswa WHERE umur <20;
+
+--NO.3 Tampilkan Mahasiswa yang memiliki nilai 'B' ke atas
+SELECT * FROM KontrakKuliah NATURAL JOIN Mahasiswa WHERE nilai < 'B';
+
+--NO.4 Tampilkan Mahasiswa yang memiliki jumlah SKS lebih dari 10
+SELECT nama_mahasiswa,sum(jumlah_sks) FROM KontrakKuliah NATURAL JOIN Matakuliah NATURAL JOIN Mahasiswa group by nim HAVING sum(jumlah_sks) > 10;
+
+--N0.5 Tampilkan mahasiswa yang mengontrak mata kuliah 'data mining'
+SELECT nama_mahasiswa,umur,nilai,nama_matakuliah,nama_dosen FROM KontrakKuliah NATURAL JOIN Mahasiswa NATURAL JOIN Matakuliah NATURAL JOIN Dosen WHERE nama_matakuliah = 'Data Mining'; 
+
+--NO.6 Tampilkan jumlah mahasiswa untuk setiap dosen
+SELECT nama_dosen,count(DISTINCT nama_mahasiswa)jumlah_mahasiswa FROM KontrakKuliah NATURAL JOIN Dosen NATURAL JOIN Mahasiswa group by id_dos;
+
+--NO.7 Urutkan Mahasiswa Berdasarkan Umurnya
+SELECT * FROM Mahasiswa ORDER BY umur asc;
+
+--NO.8 Tampilkan Kontrak Mata Kuliah yang harus Diulang (Nilai D dan E), Serta Tampilkan Data Mahasiswa Jurusan dan Dosen
+SELECT * FROM KontrakKuliah NATURAL JOIN Mahasiswa NATURAL JOIN Dosen NATURAL JOIN Matakuliah NATURAL JOIN Jurusan WHERE nilai > 'C';
